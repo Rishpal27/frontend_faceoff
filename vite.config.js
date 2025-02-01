86e0cad2b526c97e964cccf615f328270294dc21
Rishabh Rajeev Palany <rishabh.palany2708@gmail.com>
2025-02-01 11:38:59 +0530

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Allows external connections
+    port: 3000, // Change if needed
+    strictPort: true, // Ensures Vite sticks to the port
+    hmr: {
+      clientPort: 443, // Important for Render (uses HTTPS)
+    },
+  },
+});
+
